d3bf83addeb788b9d9661c1c8d1b4731b7dad992
Heryn Mendez <heryn24@gmail.com>
2021-07-13 23:35:08 -0500

    refactor:selection ahora es una función

--- selection.py
+++ selection.py
@@ -1,31 +1,29 @@
 import numpy as np
-from numpy.core.defchararray import index
 
-n = 10
-to_sort = np.random.randint(9, size = 10)
+def selectionSort(to_sort):
+    print(f'Array desordenado: {to_sort}')
+    for index_to_sort, value_to_sort in enumerate(to_sort):
 
-print(f'Array desordenado: {to_sort}')
-for index_to_sort, value_to_sort in enumerate(to_sort):
+        print(f'Array en ordenamiento: {to_sort}')
+        array_disorderly = to_sort[index_to_sort + 1:]
+        print(f'rango desordenado {array_disorderly}')
 
-    print(f'Array en ordenamiento: {to_sort}')
-    array_disorderly = to_sort[index_to_sort + 1:]
-    print(f'rango desordenado {array_disorderly}')
+        if len(array_disorderly) == 0:
+            break
 
-    if len(array_disorderly) == 0:
-        break
+        minimum_value = np.min(array_disorderly)
+        index_minimum_value = np.argmin(array_disorderly) + (index_to_sort + 1)
 
-    minimum_value = np.min(array_disorderly)
-    index_minimum_value = np.argmin(array_disorderly) + (index_to_sort + 1)
+        if value_to_sort < minimum_value:
+            minimum_value = value_to_sort
+            index_minimum_value = index_to_sort
 
-    if value_to_sort < minimum_value:
-        minimum_value = value_to_sort
-        index_minimum_value = index_to_sort
+        print(f'Posición y valor del minimo en el rango desordenado: {index_minimum_value}, {minimum_value}' )
+        # Asignar el valor de la posción 0 a la posición del minimo valor encontrado (minimun_index)
 
-    print(f'Posición y valor del minimo en el rango desordenado: {index_minimum_value}, {minimum_value}' )
-    # Asignar el valor de la posción 0 a la posición del minimo valor encontrado (minimun_index)
+        # Intercambiar el valor min encontrado (minimum_value) con el de la posción 0
+        to_sort[index_minimum_value] = value_to_sort
+        to_sort[index_to_sort] = minimum_value
 
-    # Intercambiar el valor min encontrado (minimum_value) con el de la posción 0
-    to_sort[index_minimum_value] = value_to_sort
-    to_sort[index_to_sort] = minimum_value
-
-    print(f'Iteracion # {index_to_sort}: {to_sort} \n')
+        print(f'Iteracion # {index_to_sort}: {to_sort} \n')
+    return to_sort
