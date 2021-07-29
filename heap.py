2f53854992fafec168b748c9a6776bb235ed60e7
Heryn Mendez <heryn24@gmail.com>
2021-07-28 22:34:36 -0500

    Style: Remover prints y codigo basura

--- heap.py
+++ heap.py
@@ -1,7 +1,4 @@
 import numpy as np
-from numpy.core.numeric import False_
-from numpy.lib.function_base import append
-
 
 def buildMaxHeap (to_sort):
 
@@ -22,8 +19,6 @@ def maxHeap (to_sort):
     to_sort[0] = lastBranchValue
     to_sort[-1] = rootValue
 
-    # to_sort= np.delete(to_sort, -1)
-    
     return to_sort
 
 def heapify (to_sort, order_all = False):
@@ -33,8 +28,7 @@ def heapify (to_sort, order_all = False):
     indexLastBranch = len(to_sort)
 
     while True: 
-    #for indexRootTriad in range(0,indexLastBranch):
-
+    
         indexTriad = []
         indexBranchLeft = indexRootTriad + (indexRootTriad + 1) 
         if not indexBranchLeft < indexLastBranch:
@@ -54,16 +48,8 @@ def heapify (to_sort, order_all = False):
         if maxIndexTriad != indexRootTriad:
 
             rootTriadValue = to_sort[indexRootTriad]
-
-            print(
-                f"Intercambia el {rootTriadValue} "
-                f"con el {maxValueTriad} de la triada {indexRootTriad}")
-
             to_sort [indexRootTriad] = maxValueTriad
             to_sort [maxIndexTriad] = rootTriadValue
-
-            print(to_sort)
-        
 
         if order_all:
             indexRootTriad += 1
@@ -79,31 +65,14 @@ def heap(to_sort):
     to_sort = heapify(to_sort, order_all=True)
     longitud = len(to_sort)
     order = []
-    for mimiguelito in range(0, longitud):
-        print(f'Iteracion numero {mimiguelito}, longitud to_sort {len(to_sort)}')
+    for i in range(0, longitud):
+        
         to_sort = buildMaxHeap(to_sort)
         to_sort = maxHeap(to_sort)
-        print('Antes de mimiguelito ',to_sort)
+        
         order.append(to_sort[-1])
-        to_sort = to_sort[:-1] 
-        print('Despues de mimiguelito ',to_sort)
+        to_sort = to_sort[:-1]
 
         to_sort = heapify(to_sort, order_all=False)
 
-    print(order)
-
-if __name__ == "__main__":
-
-    # n = 9
-    # to_sort = np.random.randint(n, size = n)
-    # print(f'Array original: {to_sort}')
-    # perro = buildMaxHeap(to_sort)
-    # print(f'buildMaxHeap: {perro}')
-
-    # gato = maxHeap(to_sort)
-    # print(f'MaxHeap: {gato}')
-
-    arr = [29, 40, 14, 72, 14, 23, 40, 31, 80]
-
-
-    heap(arr)
+    return order
